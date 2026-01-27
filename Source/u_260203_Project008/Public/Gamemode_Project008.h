f147633eb7f007ac51e4f33c0156a87a381d9b92
Bill Lee <ood11611doo@gmail.com>
2026-01-27 11:41:09 +0900

    260127 - Updates Initial commit after project setted

---
+++ Source/u_260203_Project008/Public/Gamemode_Project008.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Gamemode_Project008.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class U_260203_PROJECT008_API AGamemode_Project008 : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
